f43df06992cce61c6f10d258d978859608558369
nyg1 <46832635+nyg1@users.noreply.github.com>
2020-01-18 13:53:27 -0700

    Create example_page.py

---
+++ example_page.py
@@ -0,0 +1,13 @@
+import bs4
+from bs4 import BeautifulSoup as soup
+from urllib.request import urlopen as uReq
+
+course_url = "https://calendar.ualberta.ca/content.php?catoid=29&navoid=7430"
+
+uClient = uReq(course_url)
+
+course_html = uClient.read()
+uClient.close()
+
+course_parser = soup(course_html, "html.parser")
+uClient.close()
