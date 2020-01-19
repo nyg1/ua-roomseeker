7fbabf6cdb664ee8cdbb2f0a6f08e7e710001a89
nyg1 <46832635+nyg1@users.noreply.github.com>
2020-01-18 17:12:26 -0700

    Update courses_scraping.py

--- courses_scraping.py
+++ courses_scraping.py
@@ -2,9 +2,8 @@ import bs4
 from bs4 import BeautifulSoup as soup
 from urllib.request import urlopen as uReq
 
-for n in range(1, 2):
+for n in range(1, 74):
     course_url = "https://calendar.ualberta.ca/content.php?catoid=29&catoid=29&navoid=7430&filter%5Bitem_type%5D=3&filter%5Bonly_active%5D=1&filter%5B3%5D=1&filter%5Bcpage%5D={}#acalog_template_course_filter".format(n)
-    print(course_url)
 
     uClient = uReq(course_url)
 
@@ -13,7 +12,6 @@ for n in range(1, 2):
 
     course_parser = soup(course_html, "html.parser")
     uClient.close()
-
     course_list=course_parser.findAll("a")
     for h in course_list:
         print(h.text.strip())
