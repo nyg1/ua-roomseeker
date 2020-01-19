a3925f1713943b93b08c89a2a76788e6d0819472
Noah Gergel <n.gergel1@gmail.com>
2020-01-19 08:31:54 -0700

    yesseri

--- server/sql_queries.js
+++
@@ -1,8 +0,0 @@
-exports.getBuildingListSQL = `SELECT name FROM buildings;`;
-
-exports.getTimesSQL = `
-    SELECT b.bid, b.name, r.name,  FROM buildings AS b
-    INNER JOIN rooms AS r ON r.bid = b.bid
-    INNER JOIN times AS t ON t.bid = t.bid AND r.cid = t.cid
-    GROUP BY b.bid;
-`;
