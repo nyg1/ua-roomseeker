1ccd22f624fb99dc3c4921324d3dc56d0fb52e10
Noah Gergel <n.gergel1@gmail.com>
2020-02-16 19:56:03 -0700

    initialized venv

--- seeker.sh
+++ seeker.sh
@@ -2,22 +2,19 @@
 
 # This project is licenced under the MIT licence.
 
-# Starting point of a lot of the functionality.
-# Either start web scrapping, run the server, or populate the database.
-
-# Usage: ./seeker.sh [server|scrape|populate]
+# Starting point of the entire project.
+# Run the server, start web scraping, etc.
 
 
 # Global constants.
 export FLASK_APP="app/server.py"
 export FLASK_ENV="development"
 
-
-# Handle each sub command.
-case $1 in
-    server) flask run ;;
-    scrape) python3 web_scraping/scraping.py ;;
-    populate) echo "populate!" ;;
-    *) echo "Invalid command. Usage: ./seeker.sh [server|scrape|populate]"
-esac
-#flask run
+# Comment this out for now.
+# # Handle each sub command.
+# case $1 in
+#     server) flask run ;;
+#     scrape) python3 web_scraping/scraping.py ;;
+#     populate) echo "populate!" ;;
+#     *) echo "Invalid command. Usage: ./seeker.sh [server|scrape|populate]"
+# esac
